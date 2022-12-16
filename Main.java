72c6b9327d708f080582305d56fb359c42b11072
Rahul <rahulmazumder2000@gmail.com>
2022-12-16 14:01:04 +0530

    Changed the name of the project

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ package com.bank;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the banking system...");
+        System.out.println("...BANKEASY...");
+        System.out.println("One Stop solution for banking problems");
+        System.out.println("Welcome to the system...");
         start.sp();
     }
 
--- dispacc.java
+++ dispacc.java
@@ -18,30 +18,31 @@ public class dispacc {
             Connection con = c.cc1();
 
             // query..
-            String q = "select * from customer_data;";
+            String q = "select * from customer_data where (;";
+
 
             // Static query..
-            Statement s = con.createStatement();
-            ResultSet r = s.executeQuery(q);
-
-            while (r.next()){
-                int ac = r.getInt("acc_num");
-                if (ac==acc_num){
-                    System.out.println("The details of the user is given below :--");
-                    String acc_name = r.getString("acc_name");
-                    int acc_amount = r.getInt("acc_amount");
-                    int acc_pin = r.getInt("acc_pin");
-
-                    // displaying the data...
-                    System.out.println("Account holder name = " + acc_name);
-                    System.out.println("Account number = " + acc_num);
-                    System.out.println("Account pin = " + acc_pin);
-                    System.out.println("Amount present = " + acc_amount);
-
-                    break; // Since the account needed is successfully found no need to search any further..
-                }
-            }
-        }
+//            Statement s = con.createStatement();
+//            ResultSet r = s.executeQuery(q);
+//
+//            while (r.next()){
+//                int ac = r.getInt("acc_num");
+//                if (ac==acc_num){
+//                    System.out.println("The details of the user is given below :--");
+//                    String acc_name = r.getString("acc_name");
+//                    int acc_amount = r.getInt("acc_amount");
+//                    int acc_pin = r.getInt("acc_pin");
+//
+//                    // displaying the data...
+//                    System.out.println("Account holder name = " + acc_name);
+//                    System.out.println("Account number = " + acc_num);
+//                    System.out.println("Account pin = " + acc_pin);
+//                    System.out.println("Amount present = " + acc_amount);
+//
+//                    break; // Since the account needed is successfully found no need to search any further..
+//                }
+//            }
+//        }
         catch (Exception e){
             System.out.println("Sorry account not found...");
         }
